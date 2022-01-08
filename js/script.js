379b5f349583d1a7ad709c5f5d20c7f07ae135d6
maruf-mob <maruf.mob@gmail.com>
2022-01-09 01:19:29 +0600

    added carousel slider in the project

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:true,
+        navText:["<i class='fas fa-long-arrow-alt-left'></i>","<i class='fas fa-long-arrow-alt-right'></i>"],
+        autoplay:true,
+    });
+          
+});
