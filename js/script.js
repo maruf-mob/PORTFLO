1c5097a934883600aa84ecb13337a8314015c110
maruf-mob <maruf.mob@gmail.com>
2022-01-09 20:57:43 +0600

    added gallery section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    
+    //owq carousel;
     $('.owl-carousel').owlCarousel({
         loop:true,
         margin:30,
@@ -6,5 +8,21 @@ $(document).ready(function(){
         navText:["<i class='fas fa-long-arrow-alt-left'></i>","<i class='fas fa-long-arrow-alt-right'></i>"],
         autoplay:true,
     });
-          
+
+    //gallery nav li click;
+    $('.portfolio-main .navigation .port-details ul li').on('click', function(){
+        $(this).addClass('active').siblings().removeClass('active'); 
+     });
+     
+    //mixtup;
+    var mixer = mixitup('.gallery');
+
+    //gallery pop-up using venoBox;
+    
+    new VenoBox({
+        selector:'.link',
+        spinner:'flow',
+        spinColor:'#d6c6b2',
+     });
+    
 });
